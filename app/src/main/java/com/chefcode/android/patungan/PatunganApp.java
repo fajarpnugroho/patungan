1f24ff937573f0d6cc13e4f98d688427d7809c43
fajarpnugroho <fajar@kurio.co.id>
2016-02-27 03:08:09 +0700

    Saved

--- app/src/main/java/com/chefcode/android/patungan/PatunganApp.java
+++ app/src/main/java/com/chefcode/android/patungan/PatunganApp.java
@@ -6,10 +6,19 @@ import android.provider.Settings;
 
 import com.firebase.client.Firebase;
 import com.firebase.client.Logger;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPush;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushException;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushResponseListener;
+
+import java.net.MalformedURLException;
 
 import timber.log.Timber;
 
 public class PatunganApp extends Application {
+
+    private static final String APP_ROUTE = "http://Patungan.mybluemix.net";
+    private static final String APP_GUID = "0fa70150-6494-4f74-b56e-070d6eb84dd5";
 
     @Override
     public void onCreate() {
@@ -27,5 +36,30 @@ public class PatunganApp extends Application {
         Firebase.setAndroidContext(this);
         Firebase.getDefaultConfig().setLogLevel(Logger.Level.DEBUG);
         Firebase.getDefaultConfig().setPersistenceEnabled(true);
+
+        // Initialize BMSClient
+        try {
+            BMSClient.getInstance().initialize(this, APP_ROUTE, APP_GUID);
+        }
+        catch (MalformedURLException ignored) {
+        }
+
+        // Initialize Push client
+        MFPPush.getInstance().initialize(this);
+
+        MFPPush push = MFPPush.getInstance();
+
+        push.register(new MFPPushResponseListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                Timber.v("Successfully registered device for push notification");
+            }
+
+            @Override
+            public void onFailure(MFPPushException e) {
+                Timber.v("Failed registered device for push notification");
+            }
+        });
     }
+
 }
--- app/src/main/java/com/chefcode/android/patungan/ui/list/MainActivity.java
+++ app/src/main/java/com/chefcode/android/patungan/ui/list/MainActivity.java
@@ -1,8 +1,11 @@
 package com.chefcode.android.patungan.ui.list;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,16 +16,23 @@ import com.chefcode.android.patungan.R;
 import com.chefcode.android.patungan.ui.accounthistory.AccountHistoryActivity;
 import com.chefcode.android.patungan.ui.detail.PaymentDetailActivity;
 import com.chefcode.android.patungan.utils.Constants;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPush;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushNotificationListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPSimplePushNotification;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import timber.log.Timber;
 
 public class MainActivity extends BaseActivity implements MainView {
 
     @Bind(R.id.toolbar) Toolbar toolbar;
     @Bind(R.id.header_container) FrameLayout headerContainer;
     @Bind(R.id.content_container) FrameLayout contentContainer;
+
+    private MFPPush push;
+    private MFPPushNotificationListener notificationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +42,39 @@ public class MainActivity extends BaseActivity implements MainView {
         ButterKnife.bind(this);
 
         setToolbar();
+
+        notificationListener = new MFPPushNotificationListener() {
+            @Override
+            public void onReceive(final MFPSimplePushNotification message) {
+                Timber.v("Received push notif");
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Received a Push Notification")
+                                .setMessage(message.getAlert())
+                                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                    }
+                                })
+                                .show();
+                    }
+                });
+            }
+        };
+
+        push = MFPPush.getInstance();
     }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
         setContent();
+
+        if (push != null) {
+            push.listen(notificationListener);
+        }
     }
 
     private void setContent() {
